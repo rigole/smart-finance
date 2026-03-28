d33edcd5c2c536795d9203a3baa08b3b41ef0fde
Placide Rigole <foplacide@gmail.com>
2026-03-28 02:12:38 +0100

    creating controller, AuthService

---
+++ backend/src/main/java/com/smartfinance/controller/AuthController.java
@@ -0,0 +1,32 @@
+package com.smartfinance.controller;
+
+import com.smartfinance.dto.request.LoginRequest;
+import com.smartfinance.dto.request.RegisterRequest;
+import com.smartfinance.dto.response.AuthResponse;
+import com.smartfinance.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200")
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(@Valid @RequestBody RegisterRequest registerRequest) {
+        return ResponseEntity.ok(authService.register(registerRequest));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@Valid @RequestBody LoginRequest loginRequest) {
+        return ResponseEntity.ok(authService.login(loginRequest));
+    }
+
+
+
+
+}
--- backend/src/main/java/com/smartfinance/dto/request/LoginRequest.java
+++ backend/src/main/java/com/smartfinance/dto/request/LoginRequest.java
@@ -3,9 +3,10 @@ package com.smartfinance.dto.request;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.Data;
 
+@Data
 public class LoginRequest {
-
     @Email
     @NotBlank
     private String email;

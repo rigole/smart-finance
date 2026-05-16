ab0ecf1458a326ff91ed06b7d0718e38fb897086
Placide Rigole <foplacide@gmail.com>
2026-05-16 10:49:08 +0100

    removing deplicates call

--- backend/src/main/java/com/smartfinance/config/SecurityConfig.java
+++ backend/src/main/java/com/smartfinance/config/SecurityConfig.java
@@ -43,14 +43,10 @@ public class SecurityConfig {
         configuration.setAllowedMethods(List.of(
                 "GET", "POST", "PUT", "DELETE", "OPTIONS"
         ));
-        configuration.setAllowedHeaders(List.of(
-                "Authorization",
-                "Content-Type"
-        ));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
 
-        configuration.setAllowCredentials(true);
+
         UrlBasedCorsConfigurationSource source =
                 new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
